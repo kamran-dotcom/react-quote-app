ff842856efb7e20872f7e6f67de1d113b879d743
kamran-dotcom <kamichannar2@gmail.com>
2022-04-07 22:42:25 +0500

    React Quote App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [quote,setQuote] = useState("");
+  const [loading,setLoading] = useState(true);
+
+  useEffect(()=>{
+    quotes();
+  },[])
+  function quotes()
+  {
+    axios.get('https://api.adviceslip.com/advice').then(res=>{
+      console.warn(res.data.slip)
+      setQuote(res.data.slip);
+      setLoading(false);
+      console.warn(res.data.slip);
+    }).catch(error=>{
+      console.log("error",error)
+    });
+  }
+
+  function getQoute()
+  {
+    setLoading(true);
+    quotes();
+  }
+
+  // let quotes = '';
+  // if(loading)
+  // {
+  //   quotes = 'loading...';
+  // }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <h1 className="heading">{loading === true? "loading...":quote.advice}</h1>
+        <button className='btn' onClick={getQoute}>Give me Advice</button>
+      </div>
+     
     </div>
   );
 }
